70e298b7a7b5b033494507bf640153d49d0e9571
Operaconga14 <operacongahub@gmail.com>
2023-12-19 15:42:03 +0100

    Checking issues

--- api/index.py
+++ api/index.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
-from qrcodegen import generate_save
 import os
 
 app = Flask(__name__)
@@ -10,16 +9,9 @@ home = os.path.expanduser("~")
 download_path=os.path.join(home, "Downloads",)
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if generate_save():
-        success_message = "Succefully Filepath: "+download_path
-        return redirect(url_for('home', success_message=success_message))
     title="Home || QrCode Generatopr"
     qr_message = "QRCode generated will be Saved to Filepath: "+download_path
     return render_template('home.html', title=title, qr_message=qr_message)
-
-# @app.route('/about')
-# def about():
-#     return 'About'
 
 if __name__ == '__main__':
     app.run(debug=True)
